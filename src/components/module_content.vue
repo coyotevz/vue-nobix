e0c0b8d4949dc6ac6960029fd6d82ef8d73357d5
Augusto Roccasalva <augusto@rioplomo.com.ar>
2016-02-16 09:17:34 -0300

    Add some images assets, new supplier-list component

--- src/components/module_content.vue
+++ src/components/module_content.vue
@@ -1,9 +1,24 @@
 <template>
 <div class="module-content row">
   <div class="module-nav-container col s2"></div>
-  <div class="main-content col s10 offset-s2"></div>
+  <div class="main-content col s10 offset-s2">
+    <!-- sample content, this must be routed -->
+    <supplier-list></supplier-list>
+  </div>
 </div>
 </template>
 
+<script>
+var SupplierList = require('./supplier_list');
+module.exports = {
+  components: {
+    'supplier-list': SupplierList,
+  },
+};
+</script>
+
 <style lang="scss">
+.main-content {
+  margin-top: 1.66rem;
+}
 </style>
---
+++ src/components/supplier_list.vue
@@ -0,0 +1,30 @@
+<template>
+  <ul class="list suppliers-list selectable">
+    <li class="list-item" v-for="supplier in suppliers">
+      <div class="iconimg">
+        <img class="avatar-circle" src="../assets/image/photo.jpg">
+        <input id="supplier_item_{{ supplier.id }}" type="checkbox" class="filled-in"/>
+        <label for="supplier_item_{{ supplier.id }}"></label>
+      </div>
+      <div class="name"><a href="#suppliers/{{ supplier.id }}">{{ supplier.name }}</a></div>
+      <div class="col3"></div>
+      <div class="col4"></div>
+      <div class="actions"></div>
+    </li>
+  </ul>
+</template>
+
+<script>
+module.exports = {
+  data: function() {
+    return {
+      suppliers: [
+        { id: 1, name: 'Supplier 1' },
+        { id: 2, name: 'Supplier 2' },
+        { id: 3, name: 'Supplier 3' },
+        { id: 4, name: 'Supplier 4' },
+      ],
+    };
+  },
+};
+</script>
