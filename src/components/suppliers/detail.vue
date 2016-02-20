232d478660dfbe19cffc1aa6127aa357829e2ed6
Augusto Roccasalva <augusto@rioplomo.com.ar>
2016-02-20 20:01:05 -0300

    Use vue-resource to retrieve data from server on supplier detail view.

--- src/components/suppliers/detail.vue
+++ src/components/suppliers/detail.vue
@@ -1,4 +1,33 @@
 <template>
   <preloader colors="blue" size="small" class="center" v-show="$loadingRouteData"></preloader>
-  <h3>Supplier #{{ supplier.id }}</h3>
+  <h3>Supplier #{{ supplier.id }}, {{ supplier.name }}</h3>
 </template>
+
+<script>
+/* global config */
+
+module.exports = {
+
+  route: {
+    data: function(transition) {
+      this.$http.get(config.urlRoot + '/suppliers/' + transition.to.params.id).then(function(response) {
+        transition.next({
+          supplier: response.data,
+        });
+      }, function(u_response) {
+        // error callback
+      });
+    },
+  },
+
+  data: function() {
+    return {
+      supplier: Object,
+    };
+  },
+
+  components: {
+    'preloader': require('../ui/preloader-circular'),
+  },
+};
+</script>
